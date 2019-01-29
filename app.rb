8324d1aa2253411ce5d2aa9c0daf064ffa4c29de
canikwe <c.anikwe@gmail.com>
2019-01-29 09:53:17 -0500

    added pirate form and response

--- app.rb
+++ app.rb
@@ -3,7 +3,23 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      # binding.pry
+      erb :root
+    end
 
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @p_name = params["pirate"]["name"]
+      @p_weight = params["pirate"]["weight"]
+      @p_height = params["pirate"]["height"]
+      @ship_1 = params["pirate"]["ships"][0]
+      @ship_2 = params["pirate"]["ships"][1]
+      # binding.pry
+      erb :"pirates/show"
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :ship, :type, :booty
+  @@SHIP = []
+
+  def initialize(name, type, booty)
+    @name = ship
+    @type = type
+    @booty = booty
+    @@SHIP << self
+  end
+
+  def self.all
+    @@SHIP
+  end
+
+  def self.clear
+    @@SHIP.clear
+  end
+
 end
